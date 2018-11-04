c57eb08961927b42d483e124aff888f879dd3509
Vojczech <tlaskal@biomed.cas.cz>
2018-11-04 11:11:04 +0100

    plot visualization improved, download improved

--- server.R
+++ server.R
@@ -4,8 +4,6 @@ library(readr)
 library(tidyverse)
 library(Rmisc)
 library(tools)
-library(gridExtra)
-library(grid)
 
 function(input, output) {
   
@@ -48,12 +46,14 @@ function(input, output) {
     dataset_date_filtered <- dplyr::filter(dataset_date_lubridated, date_joined_lubridated >= input$date_range[1] & date_joined_lubridated <= (input$date_range[2]+1)) %>%
       select(-date_joined)
   })
- 
+
+# timepoints with Mode=0 filtered  
  dataset_date_filtered_mode_ok <- reactive({
    dataset_date_filtered <- dataset_date_filtered()
    dataset_date_filtered_mode_ok <- dplyr::filter(dataset_date_filtered, Mode == 0) %>% gather(data_type, CO2_ppm, `CO2 Soil (ppm)`, `CO2 ATM (ppm)`) %>% mutate(flux_type = case_when(data_type == "CO2 Soil (ppm)" ~ "soil", data_type == "CO2 ATM (ppm)" ~ "atmospheric"))
  })
  
+# timepoints with Mode!=0 filtered
  dataset_date_filtered_mode_bad <- reactive({
    dataset_date_filtered <- dataset_date_filtered()
    dataset_date_filtered_mode_bad <- dplyr::filter(dataset_date_filtered, Mode != 0) %>% select(date_joined_lubridated, Mode)
@@ -77,11 +77,6 @@ function(input, output) {
    temp <- mean(dataset_date_filtered_mode_ok$`Temperature (C)`)
  })
  
- # flux type switch  
- sensor <- renderText({
-   input$sensor
- })
- 
  # ggplot  
  ggplot_final <- reactive({
    dataset_date_filtered_mode_ok <- dataset_date_filtered_mode_ok()
@@ -89,28 +84,56 @@ function(input, output) {
    flux_average_atm <- flux_average_atm()
    flux_average_soil <- flux_average_soil()
    #temperature_average <- temperature_average()
-   sensor <- sensor()
    ggplot(data = dataset_date_filtered_mode_ok, aes(y = CO2_ppm, x = date_joined_lubridated)) +
-   {if (input$plot_type == "soil") {
-     geom_line(aes(colour = flux_type), size = 1.5, alpha = 0.75, subset(dataset_date_filtered_mode_ok, flux_type == "soil"))   
-   } else {}} +
    {if (input$plot_type == "atmospheric") {
      geom_line(aes(colour = flux_type), size = 1.5, alpha = 0.75, subset(dataset_date_filtered_mode_ok, flux_type == "atmospheric"))   
    } else {}} +
-   {if (input$plot_type == c("soil", "atmospheric")) {
-     geom_line(aes(colour = flux_type), size = 1.5, alpha = 0.75, subset(dataset_date_filtered_mode_ok, flux_type == "soil" | flux_type == "atmospheric"))   
+   {if (input$plot_type == "soil") {
+     geom_line(aes(colour = flux_type), size = 1.5, alpha = 0.75, subset(dataset_date_filtered_mode_ok, flux_type == "soil"))   
    } else {}} +
-  scale_color_viridis_d()
+   {if (input$plot_type == c("atmospheric", "soil")) {
+     geom_line(aes(colour = flux_type), size = 1.5, alpha = 0.75, subset(dataset_date_filtered_mode_ok, flux_type == "atmospheric" | flux_type == "soil"))   
+   } else {}} +
+  scale_color_viridis_d() +
+  {if (input$x_scale == "day") {
+    scale_x_datetime(date_breaks = "1 day")
+  } else {}} +
+  {if (input$x_scale == "week") {
+    scale_x_datetime(date_breaks = "1 week")
+  } else {}} +
+  {if (input$x_scale == "month") {
+    scale_x_datetime(date_breaks = "1 month")
+  } else {}} +
+  {if (input$x_scale == "year") {
+    scale_x_datetime(date_breaks = "1 year")
+  } else {}} +
+  labs(title = input$plot_title, subtitle = paste("mean atmosferic concentration - ", round(flux_average_atm, digits = 1), "ppm\n", "mean soil flux concentration - ", round(flux_average_soil, digits = 1), "ppm"), x = "date", y = expression("CO"[2]^{}*" (ppm)")) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, size = 10, colour = "black"), axis.text.y = element_text(size = 13, colour = "black"), axis.title = element_text(size = 14, face = "bold", colour = "black"), plot.title = element_text(size = 14, face = "bold", colour = "black"),  plot.subtitle = element_text(colour = "black"), panel.grid.minor.y = element_blank(), legend.position = "top", legend.text = element_text(size = 11, colour = "black", face = "plain"), legend.title = element_text(size = 12, colour = "black", face = "bold"), legend.key.size = unit(3, 'lines'), legend.spacing.x = unit(0.3, 'cm'), legend.direction = "horizontal")
  })
   
-# rendering  
+# rendering plot 
  output$contents1 <- renderPlot({
    ggplot_final()
  })
- output$contents2 <- renderText({
-   temperature_average()
+ 
+ # error timepoints
+ output$caption1 <- renderText({
+   "timepoints without values"
  })
-# download  
-  
+ 
+ output$contents2 <- renderTable({
+   dataset_date_filtered_mode_bad()
+ })
+ 
+# download, name of the file according to the date  
+ output$download_plot <- downloadHandler(
+     filename = function() {
+       paste("soil_flux_", input$date_range[1], "_", input$date_range[2], sep = "", ".pdf")
+     },
+   content = function(file) {
+     ggsave(file, plot = ggplot_final(), device = "pdf", dpi = 300, height = 210, width = 297, units = "mm")
+   }
+ )
   
 }
