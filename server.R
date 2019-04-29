d2cb18e362d163062d21699918d642c0a0f2253f
Vojczech <tlaskal@biomed.cas.cz>
2019-04-29 23:33:40 +0200

    plot axis and averages formatting

--- server.R
+++ server.R
@@ -101,30 +101,77 @@ function(input, output) {
    flux_average <- flux_average()
    flux_type <- flux_type()
    #temperature_average <- temperature_average()
+   {if (flux_type == "atmo_soil") {   # if atmo/soil concentration should be displayed
    ggplot(data = dataset_date_filtered_mode_ok, aes(y = CO2_ppm, x = date_joined_lubridated)) +
-   {if (flux_type == "atmo_soil") {
-     geom_line(aes(colour = flux_type), size = 1, alpha = 0.75, subset(dataset_date_filtered_mode_ok, flux_type == "atmospheric" | flux_type == "soil"))
-     } else {}} +
-   {if (flux_type == c("flux")) {
-     geom_line(aes(colour = flux_type), size = 1, alpha = 0.75, subset(dataset_date_filtered_mode_ok, flux_type == "flux"))   
-   } else {}} +
-  scale_color_viridis_d() +
-  {if (input$x_scale == "day") {
+       geom_line(aes(colour = flux_type), size = 1, alpha = 0.75, subset(dataset_date_filtered_mode_ok, flux_type == "atmospheric" | flux_type == "soil")) +
+       scale_color_viridis_d() +
+       {if (input$x_scale == "day") {
     scale_x_datetime(date_breaks = "1 day")
-  } else {}} +
-  {if (input$x_scale == "week") {
+         } else {}} +
+       {if (input$x_scale == "week") {
     scale_x_datetime(date_breaks = "1 week")
-  } else {}} +
-  {if (input$x_scale == "month") {
+         } else {}} +
+       {if (input$x_scale == "month") {
     scale_x_datetime(date_breaks = "1 month")
-  } else {}} +
-  {if (input$x_scale == "year") {
+         } else {}} +
+       {if (input$x_scale == "year") {
     scale_x_datetime(date_breaks = "1 year")
-  } else {}} +
-  labs(title = input$plot_title, subtitle = paste("mean atmospheric concentration - ", round(flux_average_atm, digits = 1), "ppm\n", "mean soil flux concentration - ", round(flux_average_soil, digits = 1), "ppm"), x = "date", y = (expression(paste(CO^2," (", mu, "mol ", m^-2, s^-1,")", sep="")))) +
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 90, size = 10, colour = "black"), axis.text.y = element_text(size = 13, colour = "black"), axis.title = element_text(size = 14, face = "bold", colour = "black"), plot.title = element_text(size = 14, face = "bold", colour = "black"),  plot.subtitle = element_text(colour = "black"), panel.grid.minor.y = element_blank(), legend.position = "top", legend.text = element_text(size = 11, colour = "black", face = "plain"), legend.title = element_text(size = 12, colour = "black", face = "bold"), legend.key.size = unit(3, 'lines'), legend.spacing.x = unit(0.3, 'cm'), legend.direction = "horizontal")
- })
+         } else {}} +
+       labs(title = input$plot_title,
+            subtitle = paste("mean atmospheric concentration - ",
+                             round(flux_average_atm, digits = 1), "ppm\n", "mean soil concentration - ",
+                             round(flux_average_soil, digits = 1), "ppm"), 
+            x = "date", 
+            y = (expression(paste(CO[2]," (ppm)", sep="")))) +
+       theme_bw() +
+       theme(axis.text.x = element_text(angle = 90, size = 10, colour = "black"), 
+             axis.text.y = element_text(size = 13, colour = "black"), 
+             axis.title = element_text(size = 14, face = "bold", colour = "black"), 
+             plot.title = element_text(size = 14, face = "bold", colour = "black"),  
+             plot.subtitle = element_text(colour = "black"), 
+             panel.grid.minor.y = element_blank(), 
+             legend.position = "top", 
+             legend.text = element_text(size = 11, colour = "black", face = "plain"), 
+             legend.title = element_text(size = 12, colour = "black", face = "bold"), 
+             legend.key.size = unit(3, 'lines'), 
+             legend.spacing.x = unit(0.3, 'cm'), 
+             legend.direction = "horizontal")
+     } else {   # if soil flux should be displayed
+       ggplot(data = dataset_date_filtered_mode_ok, aes(y = CO2_ppm, x = date_joined_lubridated)) +
+         geom_line(aes(colour = flux_type), size = 1, alpha = 0.75, subset(dataset_date_filtered_mode_ok, flux_type == "flux")) +
+         scale_color_viridis_d() +
+         {if (input$x_scale == "day") {
+         scale_x_datetime(date_breaks = "1 day")
+           } else {}} +
+         {if (input$x_scale == "week") {
+         scale_x_datetime(date_breaks = "1 week")
+           } else {}} +
+         {if (input$x_scale == "month") {
+         scale_x_datetime(date_breaks = "1 month")
+           } else {}} +
+         {if (input$x_scale == "year") {
+         scale_x_datetime(date_breaks = "1 year")
+           } else {}} +
+         labs(title = input$plot_title,
+              subtitle = paste("mean soil flux - ",
+                               round(flux_average, digits = 1), "umol/m2/s"), 
+              x = "date", 
+              y = (expression(paste(CO[2]," (", mu, "mol ", m^-2, s^-1,")", sep="")))) +
+         theme_bw() +
+         theme(axis.text.x = element_text(angle = 90, size = 10, colour = "black"), 
+               axis.text.y = element_text(size = 13, colour = "black"), 
+               axis.title = element_text(size = 14, face = "bold", colour = "black"), 
+               plot.title = element_text(size = 14, face = "bold", colour = "black"),  
+               plot.subtitle = element_text(colour = "black"), 
+               panel.grid.minor.y = element_blank(), 
+               legend.position = "none", 
+               legend.text = element_text(size = 11, colour = "black", face = "plain"), 
+               legend.title = element_text(size = 12, colour = "black", face = "bold"), 
+               legend.key.size = unit(3, 'lines'), 
+               legend.spacing.x = unit(0.3, 'cm'), 
+               legend.direction = "horizontal")
+       }}
+   })
   
 # rendering plot 
  output$contents1 <- renderPlot({
