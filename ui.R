1a2c6f03f20ec4ffd1383210e93cea0c18379010
tlaskalv <vojtech.tlaskal@bc.cas.cz>
2022-07-27 17:18:54 +0000

    ui

--- ui.R
+++ ui.R
@@ -8,27 +8,30 @@ library(Rmisc)
 library(tools)
 require(showtext)
 library(fontawesome)
+library(broom)
 
 fluidPage(
   theme = shinytheme("sandstone"),
-  title = "Soil CO\u2082 flux data",
-  plotOutput("contents1", brush = brushOpts(id = "plot_brush", direction = "x")) %>% withSpinner(type = getOption("spinner.type", default = 4)),
+  title = "Soil gas flux data",
+  plotOutput("CH4_plot", brush = brushOpts(id = "plot_brush", direction = "x")) %>% withSpinner(type = getOption("spinner.type", default = 4)),
+  plotOutput("CO2_plot") %>% withSpinner(type = getOption("spinner.type", default = 4)),
   hr(),
   fluidRow(
     column(3,
-           h3("Soil CO\u2082 flux data"),
-           h4("1."),
+           h3("Soil gas flux data"),
+           h4("1. upload"),
            fileInput('csv_data', 
                      label = list(icon("table"), "Upload csv file"),
                      accept = c('sheetName', 'header'), 
                      multiple = FALSE),
-           p(a(list(icon("github"), "Source code"), href = "https://github.com/Vojczech/Soil_flux_app", target="_blank"))
+           p(a(list(icon("github"), "Source code"), href = "https://github.com/TlaskalV/Licor_app", target="_blank"))
     ),
     column(4, 
-           h4("2.")
+           h4("2. select linear part of the upper plot"),
+           tableOutput("test")
     ),
     column(4,
-           h4("3."),
+           h4("3. linear model"),
            uiOutput("ui"),
            tableOutput("lm")
     )
