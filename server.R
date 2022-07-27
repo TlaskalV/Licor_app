facac37195dc6de6fd397bec39c3c782433b029b
tlaskalv <vojtech.tlaskal@bc.cas.cz>
2022-07-27 12:15:25 +0000

    before manual filtering

--- server.R
+++ server.R
@@ -6,13 +6,8 @@ library(tidyverse)
 library(Rmisc)
 library(tools)
 require(showtext)
-library(hrbrthemes)
 library(svglite)
 
-# download a webfont
-font_add_google(name = "Roboto Condensed", family = "Roboto Condensed",
-                regular.wt = 400, bold.wt = 700)
-showtext_auto()
 
 function(input, output) {
   
@@ -25,7 +20,9 @@ function(input, output) {
     if (is.null(infile))
       return(NULL)
     
-    read_delim(infile$datapath, delim = ",", escape_double = FALSE, col_names = TRUE, na = "NA", trim_ws = TRUE, locale = locale( tz = "UTC"))
+    read_delim(infile$datapath, delim = "\t", escape_double = FALSE, 
+               col_names = FALSE, skip = 7) %>% 
+      setNames(c("DATAH",	"SECONDS",	"NANOSECONDS",	"NDX",	'DIAG',	'REMARK',	'DATE',	'TIME',	'H2O',	'CO2',	'CH4',	'CAVITY_P',	'CAVITY_T',	'LASER_PHASE_P',	'LASER_T',	'RESIDUAL',	'RING_DOWN_TIME',	'THERMAL_ENCLOSURE_T',	'PHASE_ERROR',	'LASER_T_SHIFT',	'INPUT_VOLTAGE',	'CHK'))
   })
 
 # get name of uploaded file
@@ -37,131 +34,24 @@ function(input, output) {
   })
   
 # merge daytime columns  
-  dataset_date_joined <- reactive({
-    dataset_flux <- dataset_flux()
-    dataset_date_joined <- mutate(dataset_flux, date_joined = paste(dataset_flux$Month, dataset_flux$Day, dataset_flux$Year, dataset_flux$Time))
-  })
-  
-# lubridate merged column
   dataset_date_lubridated <- reactive({
-    dataset_date_joined <- dataset_date_joined()
-    dataset_date_lubridated <- mutate(dataset_date_joined, date_joined_lubridated = lubridate::mdy_hms(dataset_date_joined$date_joined, tz = "UTC"))
+    dataset_flux <- dataset_flux()
+    dataset_date_lubridated <- mutate(dataset_flux, date_joined_lubridated = lubridate::ymd_hms(paste(DATE, TIME), tz = "UTC"))
   })
   
 # filter by date, +1 is important due to issues with timezone setting
 # filtering is based only on days
- dataset_date_filtered <- reactive({
+  dataset_date_filtered_mode_ok <- reactive({
    dataset_date_lubridated <- dataset_date_lubridated()
-    dataset_date_filtered <- dplyr::filter(dataset_date_lubridated, date_joined_lubridated >= input$date_range[1] & date_joined_lubridated <= (input$date_range[2]+1)) %>%
-      select(-date_joined)
+   dataset_date_filtered_mode_ok <- dplyr::filter(dataset_date_lubridated, DATE >= as.vector(paste(input$date_range[1], input$time_range[1], sep = " ")) & DATE <= as.vector(paste((input$date_range[2]+1), input$time_range[2], sep = " ")))
   })
-
-# timepoints with Mode=0 filtered  
- dataset_date_filtered_mode_ok <- reactive({
-   dataset_date_filtered <- dataset_date_filtered()
-   dataset_date_filtered_mode_ok <- dplyr::filter(dataset_date_filtered, Mode == 0) %>%
-     mutate(CO2_flux = `CO2 Soil (ppm)` - `CO2 ATM (ppm)`) %>% 
-     gather(data_type, CO2_ppm, `CO2 Soil (ppm)`, `CO2 ATM (ppm)`, `CO2_flux`) %>% 
-     mutate(concentration = case_when(data_type == "CO2 Soil (ppm)" ~ "soil", data_type == "CO2 ATM (ppm)" ~ "atmospheric", data_type == "CO2_flux" ~ "flux"))
- })
- 
-# timepoints with Mode!=0 filtered
- dataset_date_filtered_mode_bad <- reactive({
-   dataset_date_filtered <- dataset_date_filtered()
-   dataset_date_filtered_mode_bad <- dplyr::filter(dataset_date_filtered, Mode != 0) %>% 
-     select(date_joined_lubridated, Mode)
- })
- 
- # average flux atmospheric  
- flux_average_atm <- reactive({
-   flux <- summarySE(dataset_date_filtered_mode_ok(), measurevar = "CO2_ppm", groupvars = "concentration", conf.interval = 0.95)
-   temp_upper <- flux[[1,3]]
- })
- 
- # average flux soil  
- flux_average_soil <- reactive({
-   flux <- summarySE(dataset_date_filtered_mode_ok(), measurevar = "CO2_ppm", groupvars = "concentration", conf.interval = 0.95)
-   temp_upper <- flux[[3,3]]
- })
- 
- # average temperature  
- temperature_average <- reactive({
-   dataset_date_filtered_mode_ok <- dataset_date_filtered_mode_ok()
-   temp <- mean(dataset_date_filtered_mode_ok$`Temperature (C)`)
- })
- 
- # average flux  
- flux_average <- reactive({
-   flux <- summarySE(dataset_date_filtered_mode_ok(), measurevar = "Flux", groupvars = "concentration", conf.interval = 0.95)
-   temp_upper <- flux[[2,3]]
- })
- 
- # flux option  
- concentration <- renderText({
-   input$plot_type
- })
  
  # ggplot  
  ggplot_final <- reactive({
    dataset_date_filtered_mode_ok <- dataset_date_filtered_mode_ok()
-   dataset_date_filtered_mode_ok$concentration <- factor(dataset_date_filtered_mode_ok$concentration, levels = c("soil", "atmospheric", "flux"))
-   flux_average_atm <- flux_average_atm()
-   flux_average_soil <- flux_average_soil()
-   flux_average <- flux_average()
-   concentration <- concentration()
-   #temperature_average <- temperature_average()
-   {if (concentration == "atmo_soil") {   # if atmo/soil concentration should be displayed
-   ggplot(data = dataset_date_filtered_mode_ok, aes(y = CO2_ppm, x = date_joined_lubridated)) +
-       geom_line(aes(colour = concentration), size = 1, alpha = 0.75, subset(dataset_date_filtered_mode_ok, concentration == "atmospheric" | concentration == "soil")) +
-       scale_color_viridis_d() +
-       {if (input$x_scale == "day") {
-    scale_x_datetime(date_breaks = "1 day")
-         } else {}} +
-       {if (input$x_scale == "week") {
-    scale_x_datetime(date_breaks = "1 week")
-         } else {}} +
-       {if (input$x_scale == "month") {
-    scale_x_datetime(date_breaks = "1 month")
-         } else {}} +
-       {if (input$x_scale == "year") {
-    scale_x_datetime(date_breaks = "1 year")
-         } else {}} +
-       labs(title = input$plot_title,
-            subtitle = paste("mean atmospheric concentration - ",
-                             round(flux_average_atm, digits = 1), "ppm CO2\n", "mean soil concentration - ",
-                             round(flux_average_soil, digits = 1), "ppm CO2"), 
-            x = "date", 
-            y = (expression(paste(CO[2]," (ppm)", sep="")))) +
-       theme_ipsum_rc() +
-       theme(legend.position = "top", 
-             legend.direction = "horizontal",
-             axis.text.x = element_text(angle = 90)) +
-       guides(fill = guide_legend(title = "concentration"))
-     } else {   # if soil flux should be displayed
-       ggplot(data = dataset_date_filtered_mode_ok, aes(y = Flux, x = date_joined_lubridated)) +
-         geom_col(aes(colour = "#440154"), alpha = 0.5) +
-         scale_color_viridis_d() +
-         {if (input$x_scale == "day") {
-         scale_x_datetime(date_breaks = "1 day")
-           } else {}} +
-         {if (input$x_scale == "week") {
-         scale_x_datetime(date_breaks = "1 week")
-           } else {}} +
-         {if (input$x_scale == "month") {
-         scale_x_datetime(date_breaks = "1 month")
-           } else {}} +
-         {if (input$x_scale == "year") {
-         scale_x_datetime(date_breaks = "1 year")
-           } else {}} +
-         labs(title = input$plot_title,
-              subtitle = paste("mean soil flux - ",
-                               round(flux_average, digits = 1), "umol/m2/s CO2"), 
-              x = "date", 
-              y = (expression(paste(CO[2]," (", mu, "mol ", m^-2, s^-1,")", sep="")))) +
-         theme_ipsum_rc() +
-         theme(legend.position = "none",
-               axis.text.x = element_text(angle = 90))
-       }}
+   ggplot(data = dataset_date_filtered_mode_ok, aes(y = CH4, x = date_joined_lubridated)) +
+     geom_point(size = 1, aes(colour = REMARK)) +
+     theme(axis.text.x = element_text(angle = 90))
    })
   
 # rendering plot 
@@ -169,35 +59,6 @@ function(input, output) {
    ggplot_final()
  })
  
-# download, name of the file according to the date  
- output$download_plot <- downloadHandler(
-     filename = function() {
-       {if (input$download_extension == "pdf") {
-         paste("soil_flux_", input$date_range[1], "_", input$date_range[2], sep = "", ".pdf")
-       } else {
-         if (input$download_extension == "png") {
-           paste("soil_flux_", input$date_range[1], "_", input$date_range[2], sep = "", ".png")
-           } else {
-             if (input$download_extension == "svg") {
-             paste("soil_flux_", input$date_range[1], "_", input$date_range[2], sep = "", ".svg")   
-             }
-             }
-         }
-         }
-       },
-   content = function(file) {
-     {if (input$download_extension == "pdf") {
-       ggsave(file, plot = ggplot_final(), device = "pdf", dpi = 300, height = 210, width = 297, units = "mm")
-       } else {
-         if (input$download_extension == "png") {
-         ggsave(file, plot = ggplot_final(), device = "png", dpi = 300, height = 210, width = 297, units = "mm", scale = 0.4)
-         } else {
-           if (input$download_extension == "svg") {
-             ggsave(file, plot = ggplot_final(), device = "svg", dpi = 300, height = 210, width = 297, units = "mm", scale = 0.4)
-           }
-         }
-       }
-     }
-     }
- )
+output$table1 <- renderTable({ dataset_date_filtered_mode_ok() })
+
 }
--- ui.R
+++ ui.R
@@ -7,13 +7,7 @@ library(tidyverse)
 library(Rmisc)
 library(tools)
 require(showtext)
-library(hrbrthemes)
 library(fontawesome)
-
-# download a webfont
-sysfonts::font_add_google(name = "Roboto Condensed", family = "Roboto Condensed",
-                regular.wt = 400, bold.wt = 700)
-showtext_auto()
 
 fluidPage(
   theme = shinytheme("sandstone"),
@@ -33,34 +27,28 @@ fluidPage(
     column(4, 
            h4("2."),
            offset = 1,
-           textInput("plot_title", 'Write plot title',
-                     placeholder = "e.g. nitrogen addition treatment"),
            dateRangeInput('date_range',
                           label = list(icon("calendar-alt"), "Filter by date"),
                           start = Sys.Date() - 3, end = Sys.Date(),
-                          separator = " to ", format = "yy/mm/dd",
+                          separator = " to ", format = "yy-mm-dd",
                           startview = 'month', language = 'en', weekstart = 1
            ),
-           radioButtons("x_scale", "Label X axis by:", 
-                        c("day", "week", "month", "year"), 
-                        inline = TRUE,
-                        selected = "week")
+           sliderInput(
+             "time_range",
+             label = "Select time",
+             min = lubridate::origin,
+             max = lubridate::origin + lubridate::days(1) - lubridate::seconds(1),
+             value = c(lubridate::origin, lubridate::origin + lubridate::days(1) - lubridate::seconds(1)),
+             step = 5 * 60,
+             timeFormat = "%H:%M",
+             timezone = "UTC",
+             ticks = TRUE
+           ),
     ),
     column(4,
            h4("3."),
-           radioButtons("plot_type", 
-                        label = list(icon("chart-area"), "Choose plot type"), 
-                        choices = c("atmospheric/soil concentration" = "atmo_soil", "soil flux" = "flux"),
-                        selected = c("flux")),
            uiOutput("ui"),
-           br(),
-           downloadButton("download_plot", 
-                          "Download final plot"),
-           radioButtons("download_extension",
-                        label = "",
-                        choices = c("pdf" = "pdf", "png" = "png", "svg" = "svg"),
-                        selected = "pdf", 
-                        inline = TRUE)
+           tableOutput("table1")
     )
   )
 )
